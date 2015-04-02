f1f1052fee771073d3d6be244841444bebdb0cd7
Craig <cav9871@saintjoe.edu>
2015-04-02 14:42:39 -0400

    This is my program for problem 3 on the exam

---
+++ prob3.js
@@ -0,0 +1,25 @@
+
+
+
+function read(num,string) {
+	
+	print('Your number is ' + num + ' and your string is ' + string);
+}
+
+
+
+
+
+var x = 0;
+var y = 0;
+   if (x == 0){
+      print('Enter a number');
+      x = readline();
+      
+   }
+    if(y == 0) {
+	 print('Enter a string');
+       	 y = readline();
+   }
+
+read(x,y);
